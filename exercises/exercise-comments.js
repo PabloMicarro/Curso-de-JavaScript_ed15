5f577b97ce30de51703df2068ebdb61728f521ec
Pablo Espinosa <pablo@humanfirst.es>
2020-10-01 09:06:57 +0200

    Resolución del ejercicio de comentarios

---
+++ exercises/exercise-comments.js
@@ -0,0 +1,10 @@
+/*
+Esto es un comentario de bloque:
+
+- Nos permite comentar cualquier número de líneas.
+- El intérprete no las ejecutará.
+- Parece perfecto para dejarte comentarios a ti mismo o a otro desarrollador.
+
+*/
+
+// En cambio esto es un comentario de una única línea
