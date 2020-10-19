3525fe4a2fdf35875ad5383905fd9503c997cd4f
Pablo Espinosa <pablo@humanfirst.es>
2020-10-19 21:50:23 +0200

    Ejercicio for-loop

---
+++ exercises/exercises-staments-folder/for-loop.js
@@ -0,0 +1,24 @@
+/*
+Añade los números 0 al 5 inclusivos, en orden ascendente al array usando un loop for.
+*/
+let array = [];
+for (let i = 0; i < 6; i++) {
+  array[i] = i;
+}
+
+/*
+Añade los número impares del 1 al 9 en el array usando loop for
+*/
+let array2 = [];
+for (let i = 1; i < 10; i += 2) {
+  array2.push(i);
+}
+
+/*
+Añade los número pares del 1 al 9 en orden descendente en el array usando loop for
+*/
+
+let array3 = [];
+for (let i = 9; i >= 1; i -= 2) {
+  array3.push(i);
+}
