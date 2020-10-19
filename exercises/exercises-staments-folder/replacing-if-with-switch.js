03b5f12bf394bccc36fab6665f02c8c76c8aade0
Pablo Espinosa <pablo@humanfirst.es>
2020-10-19 19:55:04 +0200

    correcciones

--- exercises/exercises-staments-folder/replacing-if-with-switch.js
+++ exercises/exercises-staments-folder/replacing-if-with-switch.js
@@ -6,20 +6,20 @@ function chainToSwitch(val) {
       answer = "Marley";
       break;
     }
-    case "42": {
+    case 42: {
       answer = "The Answer";
       break;
     }
-    case "1": {
+    case 1: {
       answer = "There is no #1";
       break;
     }
 
-    case "99": {
+    case 99: {
       answer = "Missed me by this much!";
       break;
     }
-    case "7": {
+    case 7: {
       answer = "Ate Nine";
       break;
     }
